cf25c03eb439e07c10b987e4db63d26bc6ea802c
wa7t <445190686@qq.com>
2022-10-27 17:44:28 +0800

    transcode

--- self-project/transcode.cc
+++ self-project/transcode.cc
@@ -1,6 +1,6 @@
 #include "transcode.h"
 
-#define logging(fmt, ...) fprintf(stderr, fmt"\n", ##__VA_ARGS__);
+#define logging(fmt, ...) fprintf(stderr, "[%s %d]" fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);
 
 static constexpr char usage_string[] = "usage:\n"
                                        "    ./transcode [-i input] {[codec options] -o output}\n"
@@ -9,35 +9,149 @@ static constexpr char usage_string[] = "usage:\n"
                                        "-codec av_codec_type\tif output code type is the same as input codec type, please set -codec copy\n"
                                        "\n";
 
-bool StreamContext::Fill(AVStream* stream)
+bool StreamContext::FillDecoder(AVStream *in_stream)
 {
-    index = stream->index;
-    stream = stream;
-    codec = avcodec_find_decoder(stream->codecpar->codec_id);
-    if(!codec){
+    index = in_stream->index;
+    stream = in_stream;
+    codec = avcodec_find_decoder(in_stream->codecpar->codec_id);
+    if (!codec)
+    {
         logging("failed to find video decoder!");
+        return false;
+    }
+    if(codec_ctx){
+        avcodec_free_context(&codec_ctx);
+    }
+    codec_ctx = avcodec_alloc_context3(codec);
+    if (!codec_ctx)
+    {
+        logging("failed to alloc memory for codec context!");
+        return false;
+    }
+    if (avcodec_parameters_to_context(codec_ctx, in_stream->codecpar) < 0)
+    {
+        logging("failed to fill codec context!");
+        return false;
+    }
+
+    return true;
+}
+
+bool StreamContext::FillEncoderCopyFrom(AVCodecParameters *in_codecpar)
+{
+    avcodec_parameters_copy(stream->codecpar, in_codecpar);
+    return true;
+}
+
+bool StreamContext::FillEncoderSetby(AVCodecContext *dec_ctx, const std::string& id)
+{
+    codec = avcodec_find_encoder_by_name(id.c_str());
+    if(!codec) {
+        logging("failed to find encoder codec");
         return false;
     }
     codec_ctx = avcodec_alloc_context3(codec);
     if(!codec_ctx){
-        logging("failed to alloc memory for codec context!");
+        logging("failed to alloc context");
         return false;
     }
-    if(avcodec_parameters_to_context(codec_ctx, stream->codecpar) < 0){
-        logging("failed to fill codec context!");
+    // video
+    codec_ctx->height = dec_ctx->height;
+    codec_ctx->width = dec_ctx->width;
+    codec_ctx->sample_aspect_ratio = dec_ctx->sample_aspect_ratio;
+    if(codec_ctx->pix_fmt){
+        codec_ctx->pix_fmt = dec_ctx->pix_fmt;
+    }
+    codec_ctx->bit_rate = 2 * 1000 * 1000;
+    codec_ctx->rc_buffer_size = 4 * 1000 * 1000;
+    codec_ctx->rc_max_rate = 2 * 1000 * 1000;
+    codec_ctx->rc_min_rate = 2 * 1000 * 1000;
+    // audio
+    codec_ctx->sample_rate = dec_ctx->sample_rate;
+    codec_ctx->sample_fmt = dec_ctx->sample_fmt;
+    codec_ctx->bit_rate = dec_ctx->bit_rate;
+
+    stream->time_base = codec_ctx->time_base = dec_ctx->time_base;
+
+    avcodec_parameters_from_context(stream->codecpar, codec_ctx);
+
+    return true;
+}
+
+bool StreamContext::OpenCodec()
+{
+    if (index != -1 && codec && codec_ctx && (avcodec_open2(codec_ctx, codec, NULL) >= 0))
+    {
+        return true;
+    }
+    else
+    {
+        logging("failed to open codec!");
         return false;
     }
 }
 
-bool StreamContext::OpenDecoder()
+bool StreamContext::Exist() const
 {
-    if(index != -1 && codec && codec_ctx && (avcodec_open2(codec_ctx, codec, NULL) >= 0)){
+    if (index != -1 && stream && codec && codec_ctx)
+    {
         return true;
     }
-    else{
-        logging("failed to open decoder!");
+    else
+    {
         return false;
     }
+}
+
+// 每种类型只记录第一个流
+bool FormatContext::FillDecoder(AVStream *stream)
+{
+    if (stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO && v_stream_ctx.index == -1)
+    {
+        return v_stream_ctx.FillDecoder(stream);
+    }
+    if (stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO && a_stream_ctx.index == -1)
+    {
+        return a_stream_ctx.FillDecoder(stream);
+    }
+    logging("this input stream [%d : %s] is not the type we need!",
+            stream->index, av_get_media_type_string(stream->codecpar->codec_type));
+    return true;
+}
+
+// copy不需要AVCodecContext
+bool FormatContext::FillEncoderCopyFrom(AVStream *in_stream)
+{
+    if (in_stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO && !v_stream_ctx.stream)
+    {
+        v_stream_ctx.stream = avformat_new_stream(avfmt, nullptr);
+        v_stream_ctx.FillEncoderCopyFrom(in_stream->codecpar);
+    }
+    if (in_stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO && !a_stream_ctx.stream)
+    {
+        a_stream_ctx.stream = avformat_new_stream(avfmt, nullptr);
+        a_stream_ctx.FillEncoderCopyFrom(in_stream->codecpar);
+    }
+    logging("this input stream [%d : %s] is not the type we need!",
+            in_stream->index, av_get_media_type_string(in_stream->codecpar->codec_type));
+    return true;
+}
+
+bool FormatContext::FillEncoderSetby(const StreamContext &in_stream_ctx, const std::string& id)
+{
+    if (in_stream_ctx.stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO && !v_stream_ctx.stream)
+    {
+        v_stream_ctx.stream = avformat_new_stream(avfmt, nullptr);
+        v_stream_ctx.FillEncoderSetby(in_stream_ctx.codec_ctx, id);
+    }
+    if (in_stream_ctx.stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO && !a_stream_ctx.stream)
+    {
+        a_stream_ctx.stream = avformat_new_stream(avfmt, nullptr);
+        a_stream_ctx.FillEncoderSetby(in_stream_ctx.codec_ctx, id);
+    }
+    logging("this input stream [%d : %s] is not the type we need!",
+            in_stream_ctx.index, av_get_media_type_string(in_stream_ctx.stream->codecpar->codec_type));
+    return true;
 }
 
 bool Transcoder::ParseParam(int argc, char **argv)
@@ -93,6 +207,12 @@ bool Transcoder::ParseParam(int argc, char **argv)
         logging("input and output is neccessary!");
         return false;
     }
+    if(v_encode_type_.empty() || v_encode_type_ == "copy"){
+        v_copy_ = true;
+    }
+    if(a_encode_type_.empty() || a_encode_type_ == "copy"){
+        a_copy_ = true;
+    }
     return true;
 }
 
@@ -103,6 +223,12 @@ bool Transcoder::Open()
         logging("failed to open input!");
         return false;
     }
+    if(!OpenOutput())
+    {
+        logging("failed to open output!");
+        return false;
+    }
+    return true;
 }
 
 bool Transcoder::OpenInput()
@@ -120,26 +246,13 @@ bool Transcoder::OpenInput()
     av_dump_format(input_fmtctx_.avfmt, 0, input_fmtctx_.file_name.c_str(), 0);
 
     // set decoder
-    for (int i = 0; i < input_fmtctx_.avfmt->nb_streams; i++)
+    for (uint32_t i = 0; i < input_fmtctx_.avfmt->nb_streams; i++)
     {
-        AVStream * stream = input_fmtctx_.avfmt->streams[i];
-        if (stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-        {
-            if(!input_fmtctx_.video_stream.Fill(stream)){
-                return false;
-            }
-        }
-        else if(stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO){
-            if(!input_fmtctx_.audio_stream.Fill(stream)){
-                return false;
-            }
-        }
-        else{
-            logging("%s is not stream type that we need", av_get_media_type_string(stream->codecpar->codec_type));
-            continue;
-        }
+        AVStream *stream = input_fmtctx_.avfmt->streams[i];
+        input_fmtctx_.FillDecoder(stream);
     }
-    if(!input_fmtctx_.video_stream.OpenDecoder() || !input_fmtctx_.audio_stream.OpenDecoder()){
+    if (!input_fmtctx_.v_stream_ctx.OpenCodec() || !input_fmtctx_.a_stream_ctx.OpenCodec())
+    {
         return false;
     }
 
@@ -149,10 +262,44 @@ bool Transcoder::OpenInput()
 bool Transcoder::OpenOutput()
 {
     avformat_alloc_output_context2(&output_fmtctx_.avfmt, nullptr, nullptr, output_fmtctx_.file_name.c_str());
-    if(!output_fmtctx_.avfmt){
+    if (!output_fmtctx_.avfmt)
+    {
         logging("failed to alloce output format context!");
         return false;
     }
+    if (input_fmtctx_.v_stream_ctx.Exist())
+    {
+        if (v_encode_type_.empty() || v_encode_type_ == "copy")
+        {
+            output_fmtctx_.FillEncoderCopyFrom(input_fmtctx_.v_stream_ctx.stream);
+        }
+        else
+        {   
+            output_fmtctx_.FillEncoderSetby(input_fmtctx_.v_stream_ctx, v_encode_type_);
+        }
+    }
+    if (input_fmtctx_.a_stream_ctx.Exist())
+    {
+        if (a_encode_type_.empty() || a_encode_type_ == "copy")
+        {
+            output_fmtctx_.FillEncoderCopyFrom(input_fmtctx_.a_stream_ctx.stream);
+        }
+        else
+        {
+            output_fmtctx_.FillEncoderSetby(input_fmtctx_.a_stream_ctx, a_encode_type_);
+        }
+    }
+
+    if(!v_copy_ && !output_fmtctx_.v_stream_ctx.OpenCodec()){
+        logging("failed to open video encoder!");
+        return false;
+    }
+    if(!a_copy_ && !output_fmtctx_.a_stream_ctx.OpenCodec()){
+        logging("failed to open audio encoder!");
+        return false;
+    }
+
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -161,6 +308,9 @@ int main(int argc, char **argv)
     if (!transcoder.ParseParam(argc, argv))
     {
         logging("param is not valid!");
+        return -1;
+    }
+    if(!transcoder.Open()){
         return -1;
     }
 }
--- self-project/transcode.h
+++ self-project/transcode.h
@@ -18,10 +18,15 @@ extern "C"
     in --> demuxer --av_packet--> decoder --av_frame--> coder --av_packet--> muxer --> out
 */
 
+// 把codec部分从avformat中提取出来自己掌控
 struct StreamContext
 {
-    bool Fill(AVStream *);
-    bool OpenDecoder();
+    bool FillDecoder(AVStream *);
+    bool FillEncoderCopyFrom(AVCodecParameters *);
+    bool FillEncoderSetby(AVCodecContext *, const std::string &);
+    bool OpenCodec();
+    bool Exist() const;
+
     int index{-1};
     AVStream *stream{nullptr};
     const AVCodec *codec{nullptr};
@@ -30,10 +35,13 @@ struct StreamContext
 
 struct FormatContext
 {
+    bool FillDecoder(AVStream *);
+    bool FillEncoderCopyFrom(AVStream *);
+    bool FillEncoderSetby(const StreamContext&, const std::string &);
     std::string file_name;
     AVFormatContext *avfmt{nullptr};
-    StreamContext video_stream;
-    StreamContext audio_stream;
+    StreamContext v_stream_ctx;
+    StreamContext a_stream_ctx;
 };
 
 class Transcoder
@@ -45,9 +53,12 @@ public:
 private:
     bool OpenInput();
     bool OpenOutput();
+
 private:
     std::string v_encode_type_;
     std::string a_encode_type_;
+    bool v_copy_{false};
+    bool a_copy_{false};
 
     FormatContext input_fmtctx_;
     FormatContext output_fmtctx_;
