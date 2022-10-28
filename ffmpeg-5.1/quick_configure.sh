ef795ea813439ddb7b2d3486a505212bfb12485e
wa7t <445190686@qq.com>
2022-10-28 15:11:41 +0800

    libx264

--- ffmpeg-5.1/quick_configure.sh
+++ ffmpeg-5.1/quick_configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+export PKG_CONFIG_PATH=`pwd`/../x264/lib/pkgconfig
+
 ./configure \
     --prefix=../build \
     --enable-shared --disable-static --enable-gpl --enable-version3 --enable-pic --enable-nonfree --disable-doc \
     --enable-debug --disable-optimizations --disable-asm --disable-stripping \
-    --enable-libx264 
-
-make clean && make -j&{nproc} && make install
+    --enable-libx264
